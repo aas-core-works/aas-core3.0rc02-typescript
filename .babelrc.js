ee0f78d129cd76912ff9259ac3ff67e7be77b3e7
eboxberger <eboxberger@phoenixcontact.com>
2023-01-18 13:36:39 +0100

    Add bundling scripts
    The current version considers only the cjs builds. This commit adapts
    the traspile (and adds the bundling process) to create esm and umd
    based bundles. Furthermore the unbundled versions of cjs and esm are
    considered here.
    
    Following scripts were added:
    
    * build:esm
    * build:cjs
    * build:bundles
    * build:declarations
    * build:all
    
    Package.json was also adapted and considers the cjs and esm module
    systems.

---
+++ .babelrc.js
@@ -0,0 +1,26 @@
+const sharedPresets = ['@babel/typescript'];
+const shared = {
+  ignore: ['src/**/*.spec.ts'],
+  presets: sharedPresets
+}
+
+module.exports = {
+  env: {
+    esmUnbundled: shared,
+    esmBundled: {
+      ...shared,
+      presets: [['@babel/preset-env', {
+        targets: "> 0.25%, not dead"
+      }], ...sharedPresets],
+    },
+    cjs: {
+      ...shared,
+      presets: [['@babel/preset-env', {
+        modules: 'commonjs'
+      }], ...sharedPresets],
+    },
+    test: {
+      presets: ['@babel/preset-env', ...sharedPresets]    
+    },
+  }
+}
